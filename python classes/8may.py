185b86a759bdf38d70e02ab719e009f7cba91c82
Ashish Gupta <ashish.gupta119962@gmail.com>
2026-05-08 15:28:01 +0530

    update

--- python classes/8may.py
+++ python classes/8may.py
@@ -8,6 +8,3 @@
 for i in range(0,15,2):
     print(i, end=" ") #end is used to print in the same line
      
-wap to takes start_point and end_point from user input
-wap to take a number from user input and print formated table
-wap  jjvj ,
