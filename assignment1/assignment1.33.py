c475a60472729d9838173f89f681315b9d18fae3
Ashish Gupta <ashish.gupta119962@gmail.com>
2026-05-14 01:59:28 +0530

    update

--- assignment1/assignment1.33.py
+++ assignment1/assignment1.33.py
@@ -4,7 +4,16 @@
 # ● Favorite Quote
 # ● Years of Experience
 # Output format:
-# ------------------------------------------------
 # Name : <Full Name>
 # Profession : <Profession>
+# Quote : <Favorite Quote>
 # Experience : <Years of Experience> years
+
+name=input("Enter your full name: ")
+profession=input("Enter your profession: ")
+quote=input("Enter your favorite quote: ")      
+experience=int(input("Enter your years of experience: "))
+print("Name :", name        )
+print("Profession :", profession)
+print("Quote :", quote)
+print("Experience :", experience, "years")
---
+++ assignment1/assignment1.34.py
@@ -0,0 +1,25 @@
+# Ask the user:
+# ● Movie Name
+# ● Viewer Name
+# ● Seat Number
+# ● Show Time
+# Output format:
+# 🎬 Movie Ticket 🎟️
+
+# Movie : <Movie Name>
+# Name : <Viewer Name>
+# Seat No : <Seat Number>
+# Time : <Show Time>
+# Enjoy the show!
+
+movie_name = input("Enter the movie name: ")
+viewer_name = input("Enter the viewer name: ")  
+seat_number = input("Enter the seat number: ")
+show_time = input("Enter the show time: ")
+
+print("🎬 Movie Ticket 🎟️")
+print("Movie :", movie_name)
+print("Name :", viewer_name)
+print("Seat No :", seat_number)
+print("Time :", show_time)
+print("Enjoy the show!")
